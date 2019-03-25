2a7dcfd396f90b8f4b36f5c4cb43dbed621aa50e
fcrios145 <fcrios145@hotmail.com>
2019-03-24 21:18:51 -0600

    move staticroot to base settings

--- Fransonal/settings_production.py
+++ Fransonal/settings_production.py
@@ -4,7 +4,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['155.138.213.155']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
