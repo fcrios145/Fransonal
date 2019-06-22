b64068a73eacbe38857c6aad77f11410861e2c57
fcrios145 <fcrios145@hotmail.com>
2019-06-21 22:54:41 -0500

    added gunicorn

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,7 @@ from fabric import task, Connection
 @task
 def deploy(c):
     c = Connection(
-        host="155.138.213.155",
+        host="206.189.216.180",
         user="kriz",
     )
     result = c.run('make -C /home/kriz/src deploy', pty=True)
