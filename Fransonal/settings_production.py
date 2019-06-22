0a490f37e9dd7b5ace0be19f1b4614a915aa57d7
fcrios145 <fcrios145@hotmail.com>
2019-06-21 22:47:06 -0500

    change ip

--- Fransonal/settings_production.py
+++ Fransonal/settings_production.py
@@ -2,7 +2,8 @@ from .settings import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['mztprogramming.com', 'www.mztprogramming.com']
+#ALLOWED_HOSTS = ['mztprogramming.com', 'www.mztprogramming.com']
+ALLOWED_HOSTS = ['206.189.216.180',]
 
 
 # Extra places for collectstatic to find static files.
