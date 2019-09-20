ba76c9219eff9af882c32042aee20cab9755e2e9
fcrios145 <fcrios145@hotmail.com>
2019-09-19 20:12:53 -0500

    recreate project

--- fabfile.py
+++
@@ -1,9 +0,0 @@
-from fabric import task, Connection
-
-@task
-def deploy(c):
-    c = Connection(
-        host="167.99.162.3",
-        user="fransonal",
-    )
-    result = c.run('make -C /home/fransonal/src deploy', pty=True)
